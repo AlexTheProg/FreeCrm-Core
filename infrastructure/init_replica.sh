bb10e0ea39575acd16a9f6d906991269a86c1bf8
Alexandru Contu <alexandru.contu99@gmail.com>
2024-05-03 13:46:02 +0300

    sparg calculatoru

--- infrastructure/init_replica.sh
+++ infrastructure/init_replica.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "$(ls -A /var/lib/postgresql/data)" ]; then
-    until pg_basebackup --pgdata=/var/lib/postgresql/data -R --slot=replication_slot --host=postgres_primary --port=5432
+    until pg_basebackup --pgdata=/var/lib/postgresql/data -R --slot=replication_slot --host=postgres_primary --port=5433
     do
         echo 'Waiting for primary to connect...'
         sleep 2s
